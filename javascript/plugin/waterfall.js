57c6743097d947aaad250f6e42a9e1c2ddd60413
haku <ngq299@gmail.com>
2025-01-06 23:15:35 +0900

    commit

--- javascript/plugin/waterfall.js
+++ javascript/plugin/waterfall.js
@@ -8,30 +8,61 @@ if (!window.myPlugin) {
 window.myPlugin.createWaterFall = function (option) {
   // デフォルトのオプション
   const defaultOption = {
-    minHGap: 10, // 水平方向の最小gap
-    minVGap: 10, // 垂直方向の最小gap
+    minGap: 10, // 画像間gap
     imgSrcs: [], // 画像のパス
     imgWidth: 220, // 画像の横幅
     container: document.body // 画像を入れるDOM
   };
   option = Object.assign({}, defaultOption, option);
+  const imgs = []; // すべての画像を格納する配列
 
   // 画像の親要素をposition: static;以外にする
   handleParent();
   // 画像を作成する
   createImgs();
-  // 各画像の座標を設定する
-  setImgPosition();
+  // 画像を並べる(画像ダウンロードは非同期のため、window.onloadの際に実行する必要がある)
+  window.onload = setImgPosition;
+
+  // ブラウザー画面サイズ変化デバウンス
+  const debounce = myPlugin.debounce(setImgPosition, 300);
+  // ブラウザー画面サイズが変化した場合
+  window.onresize = debounce;
 
   /**
    * 各画像の座標を設定する
    */
   function setImgPosition() {
-
+    const info = getHorizontalInfo();
+    // arr: 各列における次のtop値を格納した配列
+    const arr = new Array(info.number).fill(0)
+    imgs.forEach(function (img) {
+      /* 画像の座標を設定する */
+      const minTop = Math.min(...arr);
+      img.style.top = minTop + "px";
+      const index = arr.indexOf(minTop);
+      arr[index] += img.clientHeight + option.minGap;
+      img.style.left = index * (option.imgWidth + info.gap) + "px";
+    })
+    // コンテナのheightを計算する
+    option.container.style.height = Math.max(...arr) - info.gap + "px";
   }
 
   /**
-   * 画像elementを作成する関数
+   * 画像の水平方向の情報
+   */
+  function getHorizontalInfo() {
+    const obj = {};
+    // containerのclientWidth
+    obj.containerWidth = option.container.clientWidth;
+    // 横に並べられる画像の数
+    obj.number = Math.floor((obj.containerWidth + option.minGap) / (option.imgWidth + option.minGap));
+    // 画像間の横方向の隙間を計算する
+    obj.gap = (obj.containerWidth - obj.number * option.imgWidth) / (obj.number - 1);
+    return obj;
+  }
+
+  /**
+   * 画像を作成する関数
    */
   function createImgs() {
     const fragment = document.createDocumentFragment();
@@ -40,7 +71,9 @@ window.myPlugin.createWaterFall = function (option) {
       img.src = option.imgSrcs[i];
       img.style.width = option.imgWidth + "px";
       img.style.position = "absolute";
+      img.style.transition = ".5s";
       fragment.appendChild(img);
+      imgs.push(img);
     }
     option.container.appendChild(fragment);
   }
